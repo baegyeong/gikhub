b0e4ee500b37335483dffe06ba7521484e04cf5a
baegyeong <beth0929@naver.com>
2023-01-11 00:08:01 +0900

    fix: 회원가입 xml 스크롤 적용 feat: 회원가입 시 이미 가입된 이메일일때, 모든 항목을 입력하지 않았을 때 처리

--- Gikhub/app/src/main/java/com/example/gikhub/LoginActivity.kt
+++ Gikhub/app/src/main/java/com/example/gikhub/LoginActivity.kt
@@ -1,57 +1,69 @@
 package com.example.gikhub
 
+import android.app.Dialog
 import android.content.Context
+import android.content.DialogInterface
 import android.content.Intent
 import android.graphics.Rect
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.MotionEvent
 import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
 import android.widget.Toast
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.FirebaseUser
+import androidx.appcompat.app.AlertDialog
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
-    var auth : FirebaseAuth? = null
     override fun onCreate(savedInstanceState: Bundle?) {
+        val goSignUp = Intent(this, SignUpActivity::class.java)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
-        auth = FirebaseAuth.getInstance()
-        login_btn.setOnClickListener{
-            signin()
-        }
-        val goSignUp = Intent(this, SignUpActivity::class.java)
-        signup_msg2.setOnClickListener{startActivity(goSignUp)}
-    }
+        signup_msg2.setOnClickListener { startActivity(goSignUp) }
 
-    // 자동로그인
-//    override fun onStart(){
-//        super.onStart()
-//        moveMainPage(auth?.currentUser)
-//    }
-
-    fun signin(){
-        auth?.signInWithEmailAndPassword(email.text.toString(),passwd.text.toString())
-            ?.addOnCompleteListener() {
-                    task->
-                if(task.isSuccessful){
-                    // Login
-                    moveMainPage(task.result.user)
-                }else {
-                    // Show the error message
-                    Toast.makeText(this,task.exception?.message,Toast.LENGTH_LONG).show()
+        fun dialog(){
+            var dialog = AlertDialog.Builder(this)
+            dialog.setTitle("로그인 실패")
+            dialog.setMessage("유효하지 않은 계정정보입니다.\n회원가입 하시겠습니까?")
+            var dialog_listener_yes = object : DialogInterface.OnClickListener {
+                override fun onClick(p0: DialogInterface?, p1: Int) {
+                    when (p1) {
+                        DialogInterface.BUTTON_POSITIVE ->
+                            startActivity(goSignUp)
+                    }
                 }
             }
-    }
-    fun moveMainPage(user: FirebaseUser?){
-        if(user!=null){
-            startActivity(Intent(this, MainActivity::class.java))
-            finish()
+            dialog.setPositiveButton("예", dialog_listener_yes)
+            dialog.setNegativeButton("아니오", null)
+            dialog.show()
         }
 
+        fun compareInfo() {
+            val email = email.text.toString()
+            val passwd = passwd.text.toString()
+
+            // 이메일, 비밀번호 정보 가져오는 코드
+            val savedEmail: String = "0000@example.com"
+            val savedPW: String = "12345678"
+
+            // 입력한 값과 비교
+            if (email == savedEmail && passwd == savedPW) {
+                Toast.makeText(this, "환영합니다!", Toast.LENGTH_SHORT).show()
+                startActivity(Intent(this, MainActivity::class.java))
+            } else if (email != savedEmail && passwd == savedPW) {
+                Toast.makeText(this, "이메일을 다시 확인해주세요.", Toast.LENGTH_LONG).show()
+            } else if (email == savedEmail && passwd != savedPW) {
+                Toast.makeText(this, "비밀번호를 다시 확인해주세요.", Toast.LENGTH_LONG).show()
+            } else {
+                dialog()
+            }
+        }
+        login_btn.setOnClickListener {
+            compareInfo()
+        }
     }
+
     // 화면 클릭시 키보드 내리기 및 포커스 제거
     override fun dispatchTouchEvent(event: MotionEvent?): Boolean {
         if (event?.action === MotionEvent.ACTION_DOWN) {
--- Gikhub/app/src/main/java/com/example/gikhub/OnBoardingActivity.kt
+++ Gikhub/app/src/main/java/com/example/gikhub/OnBoardingActivity.kt
@@ -3,6 +3,7 @@ package com.example.gikhub
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import kotlinx.android.synthetic.main.activity_onboarding.*
 
 class OnBoardingActivity : AppCompatActivity() {
@@ -11,7 +12,7 @@ class OnBoardingActivity : AppCompatActivity() {
         setContentView(R.layout.activity_onboarding)
 
         val goLogin = Intent(this, LoginActivity::class.java)
-        login_btn.setOnClickListener{startActivity(goLogin)}
+        login_btn.setOnClickListener{ startActivity(goLogin) }
 
         val goSignUp = Intent(this, SignUpActivity::class.java)
         signup_btn.setOnClickListener{startActivity(goSignUp)}
