839f4c54fd3d87b853bf97be4ece6832e0c1d22b
baegyeong <beth0929@naver.com>
2023-01-24 03:33:48 +0900

    fix: spring 코드 제외

--- src/main/java/mergefairy/gikhub/domain/RoomType.java
+++
@@ -1,5 +0,0 @@
-package mergefairy.gikhub.domain;
-
-public enum RoomType {
-    TOGETHER_EAT,TOGETHER_BUY,
-}
--- src/main/java/mergefairy/gikhub/web/session/SessionConst.java
+++
@@ -1,5 +0,0 @@
-package mergefairy.gikhub.web.session;
-
-public class SessionConst {
-    public static final String LOGIN_USER = "loginUser";
-}
